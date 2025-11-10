ef685c6d3fbf6bd9e59951a923c90b3e5591fba4
jonathanpastora802 <jonathanpastora802@gmail.com>
2025-11-10 15:57:16 +0000

    chore: remove src/lib from tracking and add to .gitignore

--- src/lib/data.ts
+++
@@ -1,7 +0,0 @@
-import type { Product, Sale } from './types';
-
-// These arrays are intentionally left empty.
-// All application data is fetched from and managed by Firestore.
-export const products: Product[] = [];
-
-export const sales: Sale[] = [];
